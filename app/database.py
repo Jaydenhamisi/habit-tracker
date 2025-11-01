2dde14bfa521315bd98875ecfb4034485dac7683
Jaydenhamisi <jaydenhamisi12@gmail.com>
2025-11-01 15:07:23 +0000

    prepare backend for deploy

--- app/database.py
+++ app/database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "postgresql://habit_user:habit_password@localhost:5432/habit_db"
+DATABASE_URL = "postgresql://habit_db_qddt_user:kytEUDgtEZ9EAgFhs3HfIvVtXKsA8xq9@dpg-d4322rqli9vc73cglvc0-a/habit_db_qddt"
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)

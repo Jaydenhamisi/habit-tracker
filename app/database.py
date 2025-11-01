df7c07097c0316522d09cda1982dafd021c4bf07
Jaydenhamisi <jaydenhamisi12@gmail.com>
2025-11-01 15:33:20 +0000

    Update API_URL and finalize backend deployment config

--- app/database.py
+++ app/database.py
@@ -1,7 +1,11 @@
+import os
+from dotenv import load_dotenv
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "postgresql://habit_db_qddt_user:kytEUDgtEZ9EAgFhs3HfIvVtXKsA8xq9@dpg-d4322rqli9vc73cglvc0-a/habit_db_qddt"
+load_dotenv()  # <--- LOAD .env
+
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
